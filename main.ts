ed0e0f7e7f9c11baff9fceeb0f6208f3957417f5
Minta1234 <kawinphopkodsombat@gmail.com>
2025-11-27 20:05:01 +0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,56 +1,37 @@
-radio.setGroup(55)
-
 radio.onReceivedString(function (cmd) {
-
     if (cmd == "F") {
         pins.servoWritePin(AnalogPin.P0, 100)
-        pins.servoWritePin(AnalogPin.P1, 100) 
-        pins.servoWritePin(AnalogPin.P2, 100) 
-        pins.servoWritePin(AnalogPin.P8, 100) 
-    }
-
-    else if (cmd == "B") {
+        pins.servoWritePin(AnalogPin.P1, 100)
+        pins.servoWritePin(AnalogPin.P2, 100)
+        pins.servoWritePin(AnalogPin.P8, 100)
+    } else if (cmd == "B") {
         pins.servoWritePin(AnalogPin.P0, 80)
         pins.servoWritePin(AnalogPin.P1, 80)
         pins.servoWritePin(AnalogPin.P2, 80)
         pins.servoWritePin(AnalogPin.P8, 80)
-    }
-
-    else if (cmd == "L") {
+    } else if (cmd == "L") {
         pins.servoWritePin(AnalogPin.P0, 80)
         pins.servoWritePin(AnalogPin.P1, 100)
         pins.servoWritePin(AnalogPin.P2, 80)
         pins.servoWritePin(AnalogPin.P8, 100)
-    }
-
-    else if (cmd == "R") {
+    } else if (cmd == "R") {
         pins.servoWritePin(AnalogPin.P0, 100)
         pins.servoWritePin(AnalogPin.P1, 80)
         pins.servoWritePin(AnalogPin.P2, 100)
         pins.servoWritePin(AnalogPin.P8, 80)
-    }
-
-    else if (cmd == "S") {
+    } else if (cmd == "S") {
         pins.servoWritePin(AnalogPin.P0, 90)
         pins.servoWritePin(AnalogPin.P1, 90)
         pins.servoWritePin(AnalogPin.P2, 90)
         pins.servoWritePin(AnalogPin.P8, 90)
-    }
-
-    else if (cmd == "UP") {
+    } else if (cmd == "UP") {
         pins.servoWritePin(AnalogPin.P12, 120)
-    }
-
-    else if (cmd == "DOWN") {
+    } else if (cmd == "DOWN") {
         pins.servoWritePin(AnalogPin.P12, 60)
-    }
-
-    else if (cmd == "OPEN") {
+    } else if (cmd == "OPEN") {
         pins.servoWritePin(AnalogPin.P13, 120)
-    }
-
-    else if (cmd == "CLOSE") {
+    } else if (cmd == "CLOSE") {
         pins.servoWritePin(AnalogPin.P13, 60)
     }
-
 })
+radio.setGroup(67)
